9500f3cc8e78dac5a87302a7bf2fe35efbc27759
MyNameIsNeXTSTEP <madmath@bk.ru>
2025-05-19 16:44:38 +0300

    [FIX] Global typings

--- global.d.ts
+++ global.d.ts
@@ -1,40 +1,7 @@
 /**
  * @todo Remove "no-explicit-any" and implement proper types for the api
  */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-interface ICookies {
-  get: (cookieName: string) => string;
-}
-
-interface IAuthAPI {
-  login: (credentials: { username: string; password: string }) => Promise<any>;
-  signup: (userData: { username: string; password: string; email: string;[key: string]: any }) => Promise<any>;
-  logout: () => Promise<any>;
-}
-
-interface IAdminAPI {
-  getUsers: () => Promise<any[]>;
-  updateUser: (userData: { id: string | number;[key: string]: any }) => Promise<any>;
-  deleteUser: (userId: string | number) => Promise<any>;
-}
-
-interface IWebSocketAPI {
-  auth: IAuthAPI;
-  admin: IAdminAPI;
-  connect: () => WebSocketAPI;
-  getSocket: () => WebSocket | null;
-  disconnect: () => void;
-}
-
 declare global {
-  interface Window {
-    api: IWebSocketAPI;
-    Cookies: ICookies;
-  }
-  const api: IWebSocketAPI;
-  const Cookies: ICookies;
-
   namespace wsApi {
     type WSParams = Record<string, object> | undefined;
 
